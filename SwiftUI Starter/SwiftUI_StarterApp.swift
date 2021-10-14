c4d9bc516e4196bdb579197354a77808ecd69c13
Emily Liang <eliang@lcs.on.ca>
2021-10-14 09:12:23 -0400

    Initial Commit

---
+++ SwiftUI Starter/SwiftUI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StarterApp.swift
+//  SwiftUI Starter
+//
+//  Created by Emily Liang on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
